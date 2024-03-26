d75eef29af0177df002fe076061e2e15c51fd7c8
MohammadNasimi <mohammadnasimi6803@gmail.com>
2024-03-26 15:35:51 +0330

    create builder

--- builder.py
+++ builder.py
@@ -23,7 +23,7 @@ class Car: # product
         
         
 
-class Abstractbuilder(ABC):
+class Abstractbuilder(ABC): # Abstract Builder
     
     @abstractmethod
     def get_wheel(self):
@@ -36,3 +36,39 @@ class Abstractbuilder(ABC):
     @abstractmethod
     def get_body(self):
         pass
+    
+class Benz(Abstractbuilder): # concrate builder 1
+    
+    def get_body(self):
+        body = Body()
+        body.shape ='suv'
+        return body
+    def get_engine(self):
+        engine = Engine()
+        engine.hp = 123
+        return engine
+    def get_wheel(self):
+        wheel = Wheel()
+        wheel.size = 20   
+        return wheel 
+
+class Bmw(Abstractbuilder): # concrate builder 2
+    
+    def get_body(self):
+        body = Body()
+        body.shape ='m2'
+        return body
+    def get_engine(self):
+        engine = Engine()
+        engine.hp = 342
+        return engine
+    def get_wheel(self):
+        wheel = Wheel()
+        wheel.size = 23   
+        return wheel 
+        
+class Wheel: size = None
+class Engine : hp = None
+class Body : shape = None
+
+
